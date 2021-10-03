6682bc461a04746095b48cc31eedfff3f7cb578f
3dtechdimma <86073092+3dtechdima@users.noreply.github.com>
2021-10-03 21:31:54 +0300

    add HW

---
+++ src/Components/AppStules.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Main = styled.section`
+  color: #eeff00ca;
+  background-image: url(https://cs12.pikabu.ru/post_img/2021/05/19/8/1621427764178829599.jpg);
+  .message {
+    padding-bottom: 230px;
+  }
+`;
